eb449c686fb6cbd33175aaa5a316f642559c3107
unknown <jafarjallad00@gmail.com>
2024-05-04 18:54:03 +0300

    food page and responsive

---
+++ js/home.js
@@ -0,0 +1,92 @@
+let options = [
+    {
+        name: "DashBoared",
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M128 136c0-22.1-17.9-40-40-40L40 96C17.9 96 0 113.9 0 136l0 48c0 22.1 17.9 40 40 40H88c22.1 0 40-17.9 40-40l0-48zm0 192c0-22.1-17.9-40-40-40H40c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40H88c22.1 0 40-17.9 40-40V328zm32-192v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V136c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM288 328c0-22.1-17.9-40-40-40H200c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V328zm32-192v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V136c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM448 328c0-22.1-17.9-40-40-40H360c-22.1 0-40 17.9-40 40v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V328z"/></svg>'
+        ,
+        tab: "dashbored.html"
+    },
+    {
+        name: "Food & Drinks ",
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M61.1 224C45 224 32 211 32 194.9c0-1.9 .2-3.7 .6-5.6C37.9 168.3 78.8 32 256 32s218.1 136.3 223.4 157.3c.5 1.9 .6 3.7 .6 5.6c0 16.1-13 29.1-29.1 29.1H61.1zM144 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm240 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32zM272 96a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zM16 304c0-26.5 21.5-48 48-48H448c26.5 0 48 21.5 48 48s-21.5 48-48 48H64c-26.5 0-48-21.5-48-48zm16 96c0-8.8 7.2-16 16-16H464c8.8 0 16 7.2 16 16v16c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V400z"/></svg>'
+        ,
+        tab: "food.html"
+    },
+    {
+        name: "Messages",
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M208 352c114.9 0 208-78.8 208-176S322.9 0 208 0S0 78.8 0 176c0 38.6 14.7 74.3 39.6 103.4c-3.5 9.4-8.7 17.7-14.2 24.7c-4.8 6.2-9.7 11-13.3 14.3c-1.8 1.6-3.3 2.9-4.3 3.7c-.5 .4-.9 .7-1.1 .8l-.2 .2 0 0 0 0C1 327.2-1.4 334.4 .8 340.9S9.1 352 16 352c21.8 0 43.8-5.6 62.1-12.5c9.2-3.5 17.8-7.4 25.3-11.4C134.1 343.3 169.8 352 208 352zM448 176c0 112.3-99.1 196.9-216.5 207C255.8 457.4 336.4 512 432 512c38.2 0 73.9-8.7 104.7-23.9c7.5 4 16 7.9 25.2 11.4c18.3 6.9 40.3 12.5 62.1 12.5c6.9 0 13.1-4.5 15.2-11.1c2.1-6.6-.2-13.8-5.8-17.9l0 0 0 0-.2-.2c-.2-.2-.6-.4-1.1-.8c-1-.8-2.5-2-4.3-3.7c-3.6-3.3-8.5-8.1-13.3-14.3c-5.5-7-10.7-15.4-14.2-24.7c24.9-29 39.6-64.7 39.6-103.4c0-92.8-84.9-168.9-192.6-175.5c.4 5.1 .6 10.3 .6 15.5z"/></svg>'
+        ,
+        tab: "messages.html"
+    },
+    {
+        name: "Bills",
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>'
+        ,
+        tab: "bills.html"
+    },
+    {
+        name: "Settings",
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 416c0 17.7 14.3 32 32 32l54.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 448c17.7 0 32-14.3 32-32s-14.3-32-32-32l-246.7 0c-12.3-28.3-40.5-48-73.3-48s-61 19.7-73.3 48L32 384c-17.7 0-32 14.3-32 32zm128 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM320 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32-80c-32.8 0-61 19.7-73.3 48L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l246.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48l54.7 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-54.7 0c-12.3-28.3-40.5-48-73.3-48zM192 128a32 32 0 1 1 0-64 32 32 0 1 1 0 64zm73.3-64C253 35.7 224.8 16 192 16s-61 19.7-73.3 48L32 64C14.3 64 0 78.3 0 96s14.3 32 32 32l86.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 128c17.7 0 32-14.3 32-32s-14.3-32-32-32L265.3 64z"/></svg>'
+        ,
+        tab: "settings.html"
+    }
+    ,
+    {
+        name: "Notifications",
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>'
+        ,
+        tab: "notifacations.html"
+    },
+    {
+        name: "Support",
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 48C141.1 48 48 141.1 48 256v40c0 13.3-10.7 24-24 24s-24-10.7-24-24V256C0 114.6 114.6 0 256 0S512 114.6 512 256V400.1c0 48.6-39.4 88-88.1 88L313.6 488c-8.3 14.3-23.8 24-41.6 24H240c-26.5 0-48-21.5-48-48s21.5-48 48-48h32c17.8 0 33.3 9.7 41.6 24l110.4 .1c22.1 0 40-17.9 40-40V256c0-114.9-93.1-208-208-208zM144 208h16c17.7 0 32 14.3 32 32V352c0 17.7-14.3 32-32 32H144c-35.3 0-64-28.7-64-64V272c0-35.3 28.7-64 64-64zm224 0c35.3 0 64 28.7 64 64v48c0 35.3-28.7 64-64 64H352c-17.7 0-32-14.3-32-32V240c0-17.7 14.3-32 32-32h16z"/></svg>'
+        ,
+        tab: "support.html"
+    },
+];
+
+let tabnumber = 0;
+
+
+ function rendermenu() {
+    let ul = document.querySelector(".menu_list");
+    ul.innerHTML = "";
+    let ul2 = document.querySelector(".other_list");
+    ul2.innerHTML = "";
+    options.forEach( (option , index) => {
+        if(index>= 5){
+            ul2.innerHTML += `<li class="menu_list_item ${  tabnumber ==index? "active" : ""  } " onclick="opentab('${option.tab}' , ${index})">${option.svg} 
+        <h3>${option.name}</h3>
+        </li>`;
+        }
+        else {
+        ul.innerHTML += `<li class="menu_list_item ${  tabnumber ==index? "active" : ""  } " onclick="opentab('${option.tab}' , ${index})">${option.svg} 
+        <h3>${option.name}</h3>
+        </li>`;
+        }
+    });
+
+}
+
+function opentab(option , index) {
+    let iframe = document.querySelector("iframe");
+    iframe.setAttribute("src", `./${option}`);
+    tabnumber = index;
+    rendermenu();
+}
+rendermenu();
+
+window.addEventListener('resize', () => {
+    let width = window.innerWidth;
+    console.log(width);
+});
+/*function renderothermenu() {
+    let ul = document.querySelector(".other_list");
+    ul.innerHTML = "";
+    otheroptions.forEach((option, index) => {
+        ul.innerHTML += `<li class="menu_list_item ${  tabnumber ==index? "active" : ""  } " onclick="opentab('${option.tab}' , ${index})">${option.svg} 
+        <h3>${option.name}</h3>
+        </li>`;
+    });
+}*/
+
+
--- js/index.js
+++ js/index.js
@@ -6,28 +6,28 @@ checkToken();
 
 
 const swiper = new Swiper('.swiper', {
-    speed: 800,
-    spaceBetween: 100,
-    autoplay: {
-        delay: 1500,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-     
-  });
+  speed: 800,
+  spaceBetween: 100,
+  autoplay: {
+    delay: 1500,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
-  let slider = document.querySelector('.swiper .swiper-wrapper');
+});
+
+let slider = document.querySelector('.swiper .swiper-wrapper');
 let slides = [
   {
-    name: "slide1" , src : "./css/img/undraw_data_report_re_p4so.svg",
+    name: "slide1", src: "./css/img/undraw_data_report_re_p4so.svg",
   },
   {
-    name: "slide2", src : "./css/img/undraw_online_articles_re_yrkj.svg",
+    name: "slide2", src: "./css/img/undraw_online_articles_re_yrkj.svg",
   },
   {
-    name: "slide3", src : "./css/img/undraw_credit_card_re_blml.svg",
+    name: "slide3", src: "./css/img/undraw_credit_card_re_blml.svg",
   }
 ];
 
@@ -36,67 +36,87 @@ slides.forEach((slide, index) => {
 });
 
 
-  
-  let email = document.querySelector('#email');
-  let password = document.querySelector('#password');
-  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
- 
-  function logiin(event) {
-    event.preventDefault();
-    let checkbox = document.querySelector("#check");
+
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function logiin(event) {
+  event.preventDefault();
+  let checkbox = document.querySelector("#check");
   let checkval = checkbox.checked;
-    emailvalue = email.value.trim().toLowerCase();
-    passwordvalue = password.value;
-    console.log(regex.test(emailvalue));
-    if(regex.test(emailvalue)){
+  emailvalue = email.value.trim().toLowerCase();
+  passwordvalue = password.value;
+  console.log(regex.test(emailvalue));
+  if (regex.test(emailvalue)) {
 
-      if(passwordvalue.length >= 8){
-        let res = {
-          msg : "success",
-          token : 112233,
-        }
-
-        if(checkval){
-          localStorage.setItem('token', res.token);
-        }
-        else{
-          sessionStorage.setItem('token', res.token);
-        }
-        
-        console.log('valid');
-
-        window.location.href = 'home.html';
-      }else{
-        alert('invalid password');
+    if (passwordvalue.length >= 8) {
+      let res = {
+        msg: "success",
+        token: 112233,
       }
+
+      if (checkval) {
+        localStorage.setItem('token', res.token);
+      }
+      else {
+        sessionStorage.setItem('token', res.token);
+      }
+
+      console.log('valid');
+
+      window.location.href = 'home.html';
+    } else {
+      Swal.fire({
+        title: 'Error!',
+        text: 'email or password is incorrect',
+        icon: 'error',
+        confirmButtonText: 'ok',
+        customClass: {
+          confirmButton: 'confirm' 
+        }
+      })
     }
-    else{
-      alert('invalid email');
-    }
+  }
+  else {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Email or password is incorrect',
+      icon: 'error',
+      confirmButtonText: 'OK',
+      customClass: {
+        confirmButton: 'confirm' 
+      }
+    });
+    
+  }
 }
-  
-function checkToken(){
+
+function checkToken() {
   let token = localStorage.getItem('token') || sessionStorage.getItem('token');
-  if(token){
+  if (token) {
     let res = true;
-    if(res){
+    if (res) {
       window.location.href = 'home.html';
     }
-    else{
-     
+    else {
+
       localStorage.clear();
       sessionStorage.clear();
     }
   }
-  else{
-   
+  else {
+
     localStorage.clear();
     sessionStorage.clear();
   }
 }
 
 window.addEventListener('resize', () => {
-let width = window.innerWidth;
-console.log(width);
-  
+  let width = window.innerWidth;
+  console.log(width);
+  if(width < 992){
+   
+  }
+
 });
